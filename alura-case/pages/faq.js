9f8d760c2bd1eb5a3ec7812c16c6b4ca2bfc4cf8
walacefex <walacefex01@gmail.com>
2023-03-08 01:08:35 -0300

    Removendo console log e comentarios

--- alura-case/pages/faq.js
+++ alura-case/pages/faq.js
@@ -1,10 +1,8 @@
-import FAQScreen from '../src/screens/FAQScreen';
+import FAQScreen from '../src/screens/FaqScreen';
 
 export default FAQScreen;
 
 export async function getStaticProps() {
-  console.log('Em modo DEV, sempre roda! A cada acesso')
-  console.log('Roda SOMENTE em build time')
   const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
   const faq = await fetch(FAQ_API_URL)
     .then((respostaDoServidor) => {
@@ -16,7 +14,6 @@ export async function getStaticProps() {
 
   return {
     props: {
-      qualquercoisa: 'que eu passar aqui',
       faq,
     },
   };
